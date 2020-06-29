fa1bd76e4c6f0466f05674212e960ce51e68fe45
martin.lopez3.contractor <martin.lopez3.contractor@gdevtools.com>
2020-06-28 21:13:30 -0300

    Mas prolijo

--- src/index.js
+++ src/index.js
@@ -6,28 +6,27 @@
 //----------------------------ahora con react---------------------------------------
 import React from 'react'
 import ReactDom from 'react-dom'
-const nombre={
-  name:'Cachuza',
+
+const michi = {
+  name: 'Cachuza',
   tipo: 'Gato',
   imagen: 'https://www.hola.com/imagenes/estar-bien/20180925130054/consejos-para-cuidar-a-un-gatito-recien-nacido-cs/0-601-526/cuidardgatito-t.jpg?filter=w600&filter=ds75'
 }
-const opcion=1;
+const opcion = 1;
 
 function cargarObjeto(nombre, opcion) {
-  
-  if(opcion==1){
-    return `${nombre.name}`
-  } if(opcion==""){
-    return `${nombre.tipo}`
+  if (opcion == 1) {
+    return `${michi.name}`
+  } if (opcion == "") {
+    return `${michi.tipo}`
   }
 
-
 }
-const contenido=(
+const contenido = (
   <div>
-<h1>Hola {cargarObjeto(nombre,1)}</h1>
-<img src={nombre.imagen} />
-</div>
+    <h1>Hola {cargarObjeto(michi, 1)}</h1>
+    <img src={michi.imagen} />
+  </div>
 )
-const contenedor=document.getElementById('root')
-ReactDom.render(contenido,contenedor)
+const contenedor = document.getElementById('root')
+ReactDom.render(contenido, contenedor)
