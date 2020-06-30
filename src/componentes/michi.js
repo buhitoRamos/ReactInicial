98ed8c27dc469dd38a12d34a3420b2b8dff0dee8
buhito <cym.martin85@gmail.com>
2020-06-29 22:32:15 -0300

    mejorando el prototipo de webComponents

--- src/componentes/michi.js
+++ src/componentes/michi.js
@@ -1,16 +1,39 @@
 import React from 'react'
 import cachuImg from '../imagenes/caca.png'
 import './Style/Michis.css'
-class Michi extends React.Component{
+class Michi extends React.Component{    
+    constructor(props){
+        super(props);
+        //como usar atributo---------------????
+        var gto=props.gatito;
+        alert("la edad eredada es: " +gto.edad)
+
+    }
+  
+   
+   
+    eventoLoco(){
+        const loro=document.getElementById('input')
+       console.log(loro.value) 
+       
+    }
+    
+
     render(){
         return (
+            
             <div>
+                
                 <h1 className='h1'> este es un componente cachuzo</h1>
-                  <h2>{this.props.opcion}</h2>
+                <br></br> 
+    <h2>el nombre es: {this.$.gto.nombre} </h2>                             
+                <img src={cachuImg} className='img'/>
                 <br></br>
-                <img src={cachuImg} />
-
-            </div>          
+                
+                <button onClick={this.eventoLoco} className='button'>loco</button> 
+                <input id='input' className='input' placeholder='ingreso'></input>
+            </div>   
+             
             
         )
     }
--- src/index.js
+++ src/index.js
@@ -11,39 +11,23 @@ import ReactDom from 'react-dom'
 import Michi from './componentes/michi'
 import 'bootstrap/dist/css/bootstrap.css'
 
-
-//creo un objeto
-const michi = {
-  name: 'Cachuza',
-  tipo: 'Gato'
+var gatito= {
+  nombre: 'Cachuza',
+  edad: 6
 }
-var opcion = 1;
 
-function cargarObjeto(nombre, opcion) {
-  if (opcion == 1) {
-    return `${michi.name}`
-  } if (opcion == 2) {
-    return `${michi.tipo}`
-  }
 
-}
+//aca agrego mis webs components
 const contenido = (
   <div>
-    <div>
-      <br></br>
-      <input type='text' placeholder='ingreso'></input>
-      <button onClick={cargarObjeto(michi, 2)} >confirmar</button>
-
-    </div>
-    <h1>Hola {cargarObjeto(michi, 1)}</h1>
-    <h1> Es un {cargarObjeto(michi, 2)}</h1>
-    <Michi
-    opcion={opcion} />
-
+   <Michi
+   gatito={gatito} />
 
   </div>
 )
+
 const contenedor = document.getElementById('root')
 ReactDom.render(contenido, contenedor)
+
 
 
